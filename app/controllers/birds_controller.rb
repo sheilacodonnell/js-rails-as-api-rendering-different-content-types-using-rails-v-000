fda1e211637d613b8f68d472e57f46bb5df6f186
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-03 20:45:45 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -2,6 +2,6 @@ class BirdsController < ApplicationController
   def index
     @birds = Bird.all
     render 'birds/index.html.erb'
-    render plain: "Hello #{@birds[3].name}"
+    render json: birds
   end
 end
